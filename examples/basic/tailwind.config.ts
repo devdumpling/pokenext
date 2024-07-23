5cf8dc542b1a366b63c7b440ea39f9721ee4dffe
Devon Wells <devon.wells@pm.me>
2024-07-22 22:59:39 -0400

    fix: tailwind paths

--- examples/basic/tailwind.config.ts
+++ examples/basic/tailwind.config.ts
@@ -2,8 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+
+    // pkgs
+    // only other way to do this is to build and process with tailwindcss cli
+    // https://github.com/vercel/turbo/tree/main/examples/with-tailwind
+    // but that's overkill for this
+    "../../packages/ui/src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
